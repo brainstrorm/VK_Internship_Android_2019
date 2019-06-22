dea70604b8e0570c3032667bf978f868a582467e
Aroize <gipermonk@bk.ru>
2019-06-22 19:40:50 +0300

    Added swipe feature

--- app/src/main/java/com/birdyteam/vk_intership_audio_player/controller/MainPlayer.kt
+++ app/src/main/java/com/birdyteam/vk_intership_audio_player/controller/MainPlayer.kt
@@ -1,23 +1,16 @@
 package com.birdyteam.vk_intership_audio_player.controller
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.graphics.Point
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.RelativeLayout
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.MotionEventCompat
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.birdyteam.vk_intership_audio_player.R
-import java.util.*
 
 class MainPlayer : AnimatedFragment() {
 
@@ -32,7 +25,7 @@ class MainPlayer : AnimatedFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mView = inflater.inflate(R.layout.fragment_main_player, container, false)
 
-        mainContainer = mView.findViewById<RelativeLayout>(R.id.MainContainer)
+        mainContainer = mView.findViewById(R.id.MainContainer)
 
         dropDownIcon = mView.findViewById(R.id.DropDown)
         dropDownIcon.setOnTouchListener { view, motionEvent ->
