35caa9c1c95eaf57b3cd559950873064186e1ceb
Aroize <gipermonk@bk.ru>
2019-06-22 16:36:40 +0300

    Fragments implement AnimatedFragment

--- app/src/main/java/com/birdyteam/vk_intership_audio_player/controller/MainPlayer.kt
+++ app/src/main/java/com/birdyteam/vk_intership_audio_player/controller/MainPlayer.kt
@@ -1,21 +1,36 @@
 package com.birdyteam.vk_intership_audio_player.controller
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.birdyteam.vk_intership_audio_player.R
 
-class MainPlayer : Fragment() {
+class MainPlayer : AnimatedFragment() {
 
-    private lateinit var mView: View
+    override var startAnimation: Boolean = false
+    override lateinit var animation: Animation
+    override lateinit var mView: View
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mView = inflater.inflate(R.layout.fragment_main_player, container, false)
         mView.setOnClickListener {
             (activity as? ChooseFolderActivity)?.swapFragments()
         }
+
+        if(startAnimation)
+            mView.startAnimation(animation)
         return mView
+    }
+
+    override fun replaceFragments(other: AnimatedFragment) {
+        super.replaceFragments(other)
+
     }
 }
